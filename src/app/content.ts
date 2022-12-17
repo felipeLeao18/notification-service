4d3436adc1104480c8686ef4c7289de19df6fb44
felipeLeao18 <flpleao1807@gmail.com>
2022-12-17 13:58:16 -0300

    feat: initial commit

---
+++ src/app/content.ts
@@ -0,0 +1,20 @@
+export class Content {
+  private content: string;
+
+  constructor(content: string) {
+    const isContentValid = this.validateContent(content);
+
+    if (!isContentValid) {
+      throw new Error('content validation error');
+    }
+    this.content = content;
+  }
+
+  private validateContent(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+  }
+
+  get value(): string {
+    return this.content;
+  }
+}
---
+++ src/app/notification.ts
@@ -0,0 +1,54 @@
+import { Content } from './content';
+
+export interface INotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: INotificationProps;
+
+  constructor(props: INotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
+
