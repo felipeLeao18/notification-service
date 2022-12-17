8b5bc066c2ee2b3b9618d2ce4d53ef4e6565c248
felipeLeao18 <flpleao1807@gmail.com>
2022-12-17 17:48:35 -0300

    feat: add send notification

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,32 @@
+import { Content } from '../content';
+import { Notification } from '../notification';
+import { NotificationRepository } from '../repositories/notification-repository';
+
+interface ISendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface ISendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: ISendNotificationRequest,
+  ): Promise<ISendNotificationResponse> {
+    const { recipientId, content, category } = request;
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return { notification };
+  }
+}
