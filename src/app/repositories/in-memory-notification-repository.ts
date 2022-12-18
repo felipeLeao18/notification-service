1f005fbd5481ef4fcedb53a2312b6b3f30c9f414
felipeLeao18 <flpleao1807@gmail.com>
2022-12-18 19:01:03 -0300

    feat: implement in memory repository find by id and save methods

--- src/app/repositories/in-memory-notification-repository.ts
+++ src/app/repositories/in-memory-notification-repository.ts
@@ -1,9 +1,34 @@
-import { NotificationRepository } from "./notification-repository";
-import { Notification } from "@entities/notification";
+import { NotificationRepository } from './notification-repository';
+import { Notification } from '@entities/notification';
 
 export class InMemoryNotificationsRepository implements NotificationRepository {
   public notifications: Notification[] = [];
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      ({ id }) => notificationId === id,
+    );
+
+    if (!notification) {
+      return null;
+    }
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async clean() {
+    this.notifications = [];
   }
 }
