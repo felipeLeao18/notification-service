a0c139c14bb134ba00888944b9beacd4c3edab9b
felipeLeao18 <flpleao1807@gmail.com>
2022-12-18 19:00:37 -0300

    test: ensure user can cancel notification

---
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,38 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { InMemoryNotificationsRepository } from '@app/repositories/in-memory-notification-repository';
+import { randomUUID } from 'node:crypto';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found-error';
+
+describe('Cancel Notification', () => {
+  it('should throw when notification to be canceled can not be found', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() =>
+      cancelNotification.execute({
+        notificationId: randomUUID(),
+      }),
+    ).rejects.toThrow(NotificationNotFound);
+  });
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'mock_category',
+      content: new Content('mock_content'),
+      recipientId: 'example_recipient_id',
+    });
+
+    await notificationsRepository.create(notification);
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+});
