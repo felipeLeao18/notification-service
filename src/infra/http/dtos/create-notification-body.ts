b576693738d3bb520b13ba203fb3ee65fbd8fbbb
felipeLeao18 <flpleao1807@gmail.com>
2022-12-18 15:12:17 -0300

    feat: create notification bbody dto

---
+++ src/infra/http/dtos/create-notification-body.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 248)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+}
