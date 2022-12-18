cef39de566d59125a39a03c8795b33916ed8f39b
felipeLeao18 <flpleao1807@gmail.com>
2022-12-18 19:01:25 -0300

    feat: declare findById and save methods to notification repository abstract class

--- src/app/repositories/notification-repository.ts
+++ src/app/repositories/notification-repository.ts
@@ -1,5 +1,7 @@
-import { Notification } from "@entities/notification";
+import { Notification } from '@entities/notification';
 
 export abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
