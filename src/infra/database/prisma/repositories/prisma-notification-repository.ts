54a49b76e43934064b27cae7947ed76875843706
felipeLeao18 <flpleao1807@gmail.com>
2022-12-18 18:59:43 -0300

    feat: add findById and save methods to prisma repository

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -7,12 +7,17 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class PrismaNotificationRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
+  findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
   async create(notification: Notification): Promise<void> {
-
-    const raw = PrismaNotificationMapper.toPrisma(notification)
+    const raw = PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({
-      data:raw,
+      data: raw,
     });
   }
 }
