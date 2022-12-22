95821516316fa5232b50dba059a119fe4ad938fc
felipeLeao18 <flpleao1807@gmail.com>
2022-12-22 00:28:09 -0300

    test: add factory to create notification

--- src/app/use-cases/cancel-notification.spec.ts
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -1,6 +1,7 @@
 import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
 import { InMemoryNotificationsRepository } from '@app/repositories/in-memory-notification-repository';
+import { makeNotification } from '@factories/notification-factory';
 import { randomUUID } from 'node:crypto';
 import { CancelNotification } from './cancel-notification';
 import { NotificationNotFound } from './errors/notification-not-found-error';
@@ -20,11 +21,7 @@ describe('Cancel Notification', () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const notification = new Notification({
-      category: 'mock_category',
-      content: new Content('mock_content'),
-      recipientId: 'example_recipient_id',
-    });
+    const notification = new Notification(makeNotification());
 
     await notificationsRepository.create(notification);
     await cancelNotification.execute({
