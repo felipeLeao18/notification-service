81b5a270968977b059823d87f46072a0cbcf32f6
felipeLeao18 <flpleao1807@gmail.com>
2022-12-22 00:29:19 -0300

    test: ensure recipient can get notifications

---
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryNotificationsRepository } from '@app/repositories/in-memory-notification-repository';
+import { makeNotification } from '@factories/notification-factory';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    const { notifications } = await countNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
