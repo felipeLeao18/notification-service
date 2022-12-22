7184a1e8c2b18c214facdff3d7940713714f7ca4
felipeLeao18 <flpleao1807@gmail.com>
2022-12-21 21:34:09 -0300

    feat: add count many by recipient class

---
+++ src/app/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Content } from '@app/entities/content';
+import { Notification } from '@entities/notification';
+import { NotificationRepository } from '@app/repositories/notification-repository';
+
+interface ICountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface ICountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: ICountRecipientNotificationsRequest,
+  ): Promise<ICountRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return { count };
+  }
+}
