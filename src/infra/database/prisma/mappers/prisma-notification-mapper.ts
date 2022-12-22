da866e8761bc00aa39eb108fc7a734bfa2194b68
felipeLeao18 <flpleao1807@gmail.com>
2022-12-22 18:28:23 -0300

    feat: add to domain mapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,15 +1,31 @@
-import { Notification } from "@app/entities/notification";
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { Notification as NotificationRaw } from '@prisma/client';
 
 export class PrismaNotificationMapper {
-  static toPrisma(notification: Notification){
+  static toPrisma(notification: Notification) {
     return {
       id: notification.id,
       category: notification.category,
       content: notification.content.value,
       recipientId: notification.recipientId,
+      canceledAt: notification.canceledAt,
       readAt: notification.readAt,
-      createdAt: notification.createdAt
-    }
+      createdAt: notification.createdAt,
+    };
   }
 
+  static toDomain(raw: NotificationRaw): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
+  }
 }
